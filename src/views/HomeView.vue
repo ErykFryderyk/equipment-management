7daa379c83b7bf7e2f94954d5659d6475dcc3a54
ErykFryderyk <grzeniu098@gmail.com>
2023-11-04 15:59:58 +0100

    removed comments lines

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
-    <!-- <DevicesList :users="users"/> -->
+    
   </div>
 </template>
 
 <script>
-// import DevicesList from '@/components/DevicesList.vue';
+
 </script>
