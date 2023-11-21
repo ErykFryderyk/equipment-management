d3497c6b7d33692f068ea8b3d3fd36c6f33ca1a0
ErykFryderyk <grzeniu098@gmail.com>
2023-11-21 22:48:54 +0100

    created basic components to test

---
+++ src/components/addNewPrinter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- DODAWANIE DRUKARKI DO BAZY -->
+    <h2>Dodaj drukarkę</h2>
+    <form @submit.prevent="addPrinter">
+      <div>
+        <label for="printerName">Printer Name:</label>
+        <input type="text" id="printerName" placeholder="KON1L001" />
+      </div>
+      <div>
+        <label for="model">Model:</label>
+        <select id="model">
+          <option value="">--brak--</option>
+          <option value="QLn620">QLn620</option>
+          <option value="QLn220">QLn220</option>
+          <option value="ZQ630">ZQ630</option>
+        </select>
+      </div>
+      <div>
+        <label for="printerSerialNumber">Serial Number:</label>
+        <input type="text" id="printerSerialNumber" placeholder="S1203120312031" />
+      </div>
+      <button type="submit">Dodaj skaner</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addNewPrinter',
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/addNewScanner.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>Dodaj skaner</h2>
+    <form @submit.prevent="addScanner">
+      <div>
+        <label for="scannerName">Scanner Name:</label>
+        <input type="text" id="scannerName" maxlength="8" placeholder="KON1S001" />
+      </div>
+      <div>
+        <label for="model">Model:</label>
+        <select id="model">
+          <option value="">--brak--</option>
+          <option value="TC52">TC52</option>
+          <option value="MC33">MC33</option>
+          <option value="MC55">MC55</option>
+          <option value="MC67">MC67</option>
+        </select>
+      </div>
+      <div>
+        <label for="scannerSerialNumber">Serial Number:</label>
+        <input type="text" id="scannerSerialNumber" placeholder="S7265638782641" />
+      </div>
+      <button type="submit">Dodaj skaner</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addScanner',
+  methods: {
+    addScanner() {
+      console.log("Funkcja z komponentu addScanner");
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
