616f8f5f85ca68793c2f3a04fa9e58e8681665bd
ErykFryderyk <grzeniu098@gmail.com>
2023-11-21 22:48:05 +0100

    added to modal toggle test components

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,30 +1,50 @@
 <template>
   <div class="overlay" @click="someEvent">
     <div class="modal">
+      <button @click="showComponent('addNewScanner')">Pokaż Komponent 1</button>
+      <button @click="showComponent('addNewPrinter')">Pokaż Komponent 2</button>
 
+      <!-- Wyświetlanie odpowiedniego komponentu w zależności od aktualnego stanu -->
+      <component :is="currentComponent" />
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Modal',
-    methods:{
-      someEvent(event){
-        const modalClass = event.target.classList.value;  
+import addNewScanner from './addNewScanner.vue'
+import addNewPrinter from './addNewPrinter.vue'
 
-        if(modalClass === 'overlay'){
-          this.$emit('passEvent');
-        } else {
-          return;
-        }
-      },
-    }
+export default {
+  name: 'Modal',
+  components: {
+    addNewScanner,
+    addNewPrinter,
+  },
+  data() {
+    return {
+      currentComponent: null,
+    };
+  },
+  methods: {
+    someEvent(event) {
+      const modalClass = event.target.classList.value;
+
+      if (modalClass === 'overlay') {
+        this.$emit('passEvent');
+      } else {
+        return;
+      }
+    },
+    showComponent(componentName) {
+      // Ustawia aktualny komponent na ten, który ma być widoczny
+      this.currentComponent = componentName;
+    },
   }
+}
 </script>
 
 <style lang="scss" scoped>
-.overlay{
+.overlay {
   position: absolute;
   top: 0;
   left: 0;
@@ -36,16 +56,17 @@
   background-color: #ffffffa6;
   z-index: 999;
 }
-.modal{
+
+.modal {
   width: 300px;
   min-height: 300px;
   background-color: #fff;
   box-shadow: 0 0 10px 0px #000;
   border-radius: 5px;
 
-  @media (min-width: 520px){
+  @media (min-width: 520px) {
     width: 500px;
-    min-height:500px;
+    min-height: 500px;
 
   }
 }
