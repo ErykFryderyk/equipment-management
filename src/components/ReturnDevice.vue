d888047c9857acbc2d6c78956cd20734d9b8a954
ErykFryderyk <grzeniu098@gmail.com>
2023-11-22 22:42:55 +0100

    created component to returned devices to user

---
+++ src/components/ReturnDevice.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- ZDAWANIE SPRZĘTU -->
+    <h2>Zdawanie urządzeń</h2>
+    <form @submit.prevent="returnDevices">
+      <input type="text" placeholder="login uzytkownika">
+      <input type="text" placeholder="Skaner">
+      <input type="text" placeholder="Drukarka">
+      <button type="submit">Przydziel urządzenie</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReturnDevice'
+}
+</script>
+
+<style lang="scss" scoped></style>
