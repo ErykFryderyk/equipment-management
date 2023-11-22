6a30c5ed66004a1aa185df6b0ffde97818daa730
ErykFryderyk <grzeniu098@gmail.com>
2023-11-22 22:42:18 +0100

    created component to removed devices to user

---
+++ src/components/RemoveUser.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- USUWANIE UŻYTKOWNIKA  -->
+    <h2>Usuń użytkownika</h2>
+      <form @submit.prevent="removeUserByLogin">
+        <div>
+          <label for="login">Login:</label>
+          <input type="text" id="login" v-model="existingUser" />
+        </div>
+        <button type="submit">Usuń użytkownika</button>
+      </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'RemoveUser'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
