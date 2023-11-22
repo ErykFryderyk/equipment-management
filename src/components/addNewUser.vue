4af1f69bc13b6ba1e0c556a86a4fa681e86a95f3
ErykFryderyk <grzeniu098@gmail.com>
2023-11-22 22:25:26 +0100

    added componet with form addition a new user

---
+++ src/components/addNewUser.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- DODAWANIE NOWEGO UŻYTKOWNIKA -->
+    <h2>Dodaj nowego użytkownika</h2>
+      <form @submit.prevent="addUser">
+        <div>
+          <label for="login">Login:</label>
+          <input type="text" id="login" />
+        </div>
+        <div>
+          <label for="name">Imię i nazwisko:</label>
+          <input type="text" id="name" />
+        </div>
+        <button type="submit">Dodaj użytkownika</button>
+      </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'addNewUser',
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
