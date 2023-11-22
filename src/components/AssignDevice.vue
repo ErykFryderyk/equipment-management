7f8381e42433aff03bf76612a6a1fb845e20df36
ErykFryderyk <grzeniu098@gmail.com>
2023-11-22 22:41:46 +0100

    created component to assigned devices to user

---
+++ src/components/AssignDevice.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- WYDAWANIE SPRZĘTU -->
+    <h2>Wydawanie urządzeń</h2>
+      <form @submit.prevent="assignDevices">
+        <input type="text" v-model="userLogin" placeholder="login uzytkownika">
+        <input type="text" v-model="selectedScanner" placeholder="Skaner">
+        <input type="text" v-model="selectedPrinter" placeholder="Drukarka">
+        <button type="submit">Przydziel urządzenie</button>
+      </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AssignDevice',
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
