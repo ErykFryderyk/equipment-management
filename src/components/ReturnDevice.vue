1e831c77ea8bcf17c1ad92a428fc4a65259065a7
ErykFryderyk <grzeniu098@gmail.com>
2024-01-19 22:08:18 +0100

    Added validation to form

--- src/components/ReturnDevice.vue
+++ src/components/ReturnDevice.vue
@@ -3,29 +3,90 @@
     <!-- ZDAWANIE SPRZĘTU -->
     <h2>Zdawanie urządzeń</h2>
     <form @submit.prevent="returnDevices">
-      <input type="text" v-model="userLogin" placeholder="G">
-      <!-- <input type="text" v-model="formData.selectedScanner" placeholder="Skaner"> -->
-      <!-- <input type="text" v-model="formData.selectedPrinter" placeholder="Drukarka"> -->
+      <!-- error messages -->
+      <span v-if="userLoginError" class="error">Login musi mieć co najmniej 6 znaków.</span>
+      <span v-if="userLoginEmptyError" class="error">Login nie może być pusty.</span>
+      <span v-if="scannerNameError" class="error">Nazwa skanera musi zawierać "KON1S".</span>
+      <span v-if="scannerNameEmptyError" class="error">Nazwa skanera nie może być pusta.</span>
+      <span v-if="printerNameError" class="error">Nazwa drukarki musi zawierać "KON1L".</span>
+      <span v-if="printerNameEmptyError" class="error">Nazwa drukarki nie może być pusta.</span>
+
+      <input type="text" maxlength="6" v-model="formData.userLogin" placeholder="Login pracownika">
+      <input type="text" maxlength="8" v-model="formData.selectedScanner" placeholder="Nazwa skanera">
+      <input type="text" maxlength="8" v-model="formData.selectedPrinter" placeholder="Nazwa Drukarka">
       <button type="submit">Zwróć urządzenia</button>
     </form>
+    <!-- <div class="timer"> -->
+    <!-- <ProgressBar/> -->
+    <!-- </div> -->
   </div>
 </template>
 
 <script>
+import ProgressBar from './ProgressBar.vue';
+
 export default {
   name: 'ReturnDevice',
+  components: {
+    ProgressBar,
+  },
   data() {
     return {
-      userLogin: 'G',
+      formData: {
+        userLogin: 'grzkos',
+        selectedScanner: 'kon1s001',
+        selectedPrinter: 'kon1l001',
+      },
+      userLoginError: false,
+      scannerNameError: false,
+      printerNameError: false,
     }
   },
   methods: {
     returnDevices() {
-      console.log(this.userLogin);
-      this.$emit('returnDevices', this.userLogin);
+      // this.checkEmptyInput();
+
+      const login = this.formData.userLogin.toUpperCase();
+      const scannerName = this.formData.selectedScanner.toUpperCase();
+      const printerName = this.formData.selectedPrinter.toUpperCase();
+
+      // Walidacja przed zwróceniem urządzeń
+      this.userLoginError = login.length < 5;
+      this.userLoginEmptyError = login === ''
+      this.scannerNameError = !scannerName.includes('KON1S');
+      this.scannerNameEmptyError = scannerName === '';
+      this.printerNameError = !printerName.includes('KON1L');
+      this.printerNameEmptyError = printerName === '';
+
+
+      // Sprawdzenie, czy wszystkie warunki walidacji są spełnione
+      if (!this.userLoginError && !this.scannerNameError && !this.printerNameError) {
+        this.formData.userLogin = '';
+        this.formData.selectedScanner = '';
+        this.formData.selectedPrinter = '';
+
+        this.$emit('returnDevices', login);
+      };
     }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+form {
+  padding: 20px 100px;
+  display: flex;
+  justify-content: center;
+  align-items: space-between;
+  flex-direction: column;
+}
+
+input {
+  margin-bottom: 30px;
+}
+
+.error {
+  color: red;
+  font-size: 12px;
+}
+</style>
