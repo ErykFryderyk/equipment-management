44a9f18b2269372d13ad05ac556610919843e716
ErykFryderyk <grzeniu098@gmail.com>
2024-01-24 23:25:01 +0100

    added validation to the form

--- src/components/AssignDevice.vue
+++ src/components/AssignDevice.vue
@@ -3,11 +3,22 @@
     <!-- WYDAWANIE SPRZĘTU -->
     <h2>Wydawanie urządzeń</h2>
     <form @submit.prevent="assignDevices">
-      <input type="text" v-model="assignData.userLogin" placeholder="login uzytkownika">
-      <input type="text" v-model="assignData.selectedScanner" placeholder="Skaner">
-      <input type="text" v-model="assignData.selectedPrinter" placeholder="Drukarka">
+      <!-- error messages -->
+      <span v-if="userLoginError" class="error">Login musi mieć co najmniej 6 znaków.</span>
+      <span v-if="userLoginEmptyError" class="error">Login nie może być pusty.</span>
+      <span v-if="scannerNameError" class="error">Nazwa skanera musi zawierać "KON1S***".</span>
+      <span v-if="scannerNameEmptyError" class="error">Nazwa skanera nie może być pusta.</span>
+      <span v-if="printerNameError" class="error">Nazwa drukarki musi zawierać "KON1L".</span>
+      <span v-if="printerNameEmptyError" class="error">Nazwa drukarki nie może być pusta.</span>
+
+      <input type="text" maxlength="6" v-model="formData.user" placeholder="Login pracownika">
+      <input type="text" maxlength="8" v-model="formData.scanner" placeholder="Nazwa skanera">
+      <input type="text" maxlength="8" v-model="formData.printer" placeholder="Nazwa drukarka">
       <button type="submit">Przydziel urządzenie</button>
     </form>
+    <!-- <div class="timer"> -->
+    <!-- <ProgressBar/> -->
+    <!-- </div> -->
   </div>
 </template>
 
@@ -16,19 +27,56 @@ export default {
   name: 'AssignDevice',
   data() {
     return {
-      assignData: {
-        userLogin: '',
-        selectedScanner: '',
-        selectedPrinter: '',
+      formData: {
+        user: 'KAMKOW',
+        scanner: 'KON1S002',
+        printer: 'KON1L002',
       }
     }
   },
   methods: {
     assignDevices() {
-      this.$emit('assignDevices', this.assignData);
+      const login = this.formData.user.toUpperCase();
+      const scannerName = this.formData.scanner.toUpperCase();
+      const printerName = this.formData.printer.toUpperCase();
+
+      // Walidacja przed zwróceniem urządzeń
+      this.userLoginError = login.length < 5;
+      this.userLoginEmptyError = login === ''
+      this.scannerNameError = !scannerName.includes('KON1S');
+      this.scannerNameEmptyError = scannerName === '';
+      this.printerNameError = !printerName.includes('KON1L');
+      this.printerNameEmptyError = printerName === '';
+
+      // Walidacja formularza
+      // Sprawdzenie, czy wszystkie warunki walidacji są spełnione...
+      if (!this.userLoginError && !this.scannerNameError && !this.printerNameError) {
+        // this.$emit('returnDevices', [login,scannerName,printerName]);
+        this.$emit('assignDevices', this.formData);
+      };
+
+
+
     }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+form {
+  padding: 20px 100px;
+  display: flex;
+  justify-content: center;
+  align-items: space-between;
+  flex-direction: column;
+}
+
+input {
+  margin-bottom: 30px;
+}
+
+.error {
+  color: red;
+  font-size: 12px;
+}
+</style>
