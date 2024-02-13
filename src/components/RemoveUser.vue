6c8b1afe2af1cc65f202d9dc687b276e1b5a9139
ErykFryderyk <grzeniu098@gmail.com>
2024-02-13 18:16:38 +0100

    added better validation to the form

--- src/components/RemoveUser.vue
+++ src/components/RemoveUser.vue
@@ -3,9 +3,14 @@
     <!-- USUWANIE UŻYTKOWNIKA  -->
     <h2>Usuń użytkownika</h2>
     <form @submit.prevent="removeUserByLogin">
+      <span v-if="!loginValid" style="color: red;">Login musi mieć dokładnie 6 znaków. (np. JANKOW)</span><br>
       <div>
         <label for="login">Login:</label>
-        <input type="text" id="login" v-model="existingUser" />
+        <input type="text" maxlength="6" id="login" v-model="existingUser" />
+      </div>
+      <div>
+        <label for="password">Hasło:</label>
+        <input type="password" id="password" v-model="passwordToRemoveUser"/>
       </div>
       <button type="submit">Usuń użytkownika</button>
     </form>
@@ -18,11 +23,23 @@ export default {
   data() {
     return {
       existingUser: '',
+      loginValid: true,
+      adminPassowrd: '1234',
+      passwordToRemoveUser: '1234',
     }
+  },
+  watch: {
+    'existingUser': function (newVal) {
+      this.loginValid = newVal.length === 6 && /^[a-zA-Z]+$/.test(newVal);
+    },
   },
   methods: {
     removeUserByLogin() {
-      this.$emit("removeUser", this.existingUser);
+      if (this.adminPassowrd === this.passwordToRemoveUser) {
+        this.$emit("removeUser", this.existingUser);
+      } else { 
+        alert("Hasło jest niepoprawne!");
+      }
     }
   },
 }
