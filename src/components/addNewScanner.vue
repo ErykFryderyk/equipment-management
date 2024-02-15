afdc441a00542a04a21a4bf1bf09405823677d15
ErykFryderyk <grzeniu098@gmail.com>
2024-02-15 22:47:07 +0100

    added validation to the form

--- src/components/addNewScanner.vue
+++ src/components/addNewScanner.vue
@@ -2,14 +2,17 @@
   <div>
     <h2>Dodaj skaner</h2>
     <form @submit.prevent="addScanner">
+      <span v-if="!nameValid" style="color: red;">Nazwa musi zawierać 8 znaków.</span>
+      <span v-if="!modelValid" style="color: red;">Pole "Model" nie moze byc puste</span>
+      <span v-if="!serialValid" style="color: red;">Numer seryjny do wypełnienia</span>
       <div>
-        <label for="scannerName">Scanner Name:</label>
-        <input type="text" id="scannerName" maxlength="8" placeholder="KON1S001" v-model="scanner.scannerName" />
+        <label for="scannerName" :style="{ color: nameValid ? 'green' : 'red' }">Scanner Name:</label>
+        <input type="text" id="scannerName" maxlength="10" placeholder="KON1S001" v-model="scanner.scannerName"/>
       </div>
       <div>
-        <label for="model">Model:</label>
+        <label for="model" :style="{ color: modelValid ? 'green' : 'red' }">Model:</label>
         <select id="model" v-model="scanner.model">
-          <option value="">--brak--</option>
+          <option value>--brak--</option>
           <option value="TC52">TC52</option>
           <option value="MC33">MC33</option>
           <option value="MC55">MC55</option>
@@ -17,7 +20,7 @@
         </select>
       </div>
       <div>
-        <label for="scannerSerialNumber">Serial Number:</label>
+        <label for="scannerSerialNumber" :style="{ color: serialValid ? 'green' : 'red' }">Serial Number:</label>
         <input type="text" id="scannerSerialNumber" placeholder="S7265638782641" v-model="scanner.serialNumber" />
       </div>
       <button type="submit">Dodaj skaner</button>
@@ -34,13 +37,32 @@ export default {
         scannerName: 'KON1S',
         model: '',
         serialNumber: 'S123123',
-
-      }
+      },
+      nameValid: false,
+      modelValid: false,
+      serialNumber: false,
     }
+  },
+  watch: {
+    'scanner.scannerName': function (newVal) {
+      this.nameValid = newVal.length === 8;
+    },
+    'scanner.model': function (newVal) {
+      this.modelValid = newVal.lenght != '';
+    },
+    'scanner.serialNumber': function (newVal) {
+      this.serialValid = newVal.length >= 12;
+    },
   },
   methods: {
     addScanner() {
-      this.$emit('addNewScanner', this.scanner);
+      // Walidacja przed zwróceniem urządzeń
+      if(this.nameValid && this.modelValid && this.serialValid) {
+        this.$emit('addNewScanner', this.scanner);
+      } else{
+        alert("wypełnij poprawnie pola");
+        return;
+      }
     }
   }
 }
