298bbbe3e370159b71c753abea59f82de3f8089f
ErykFryderyk <grzeniu098@gmail.com>
2024-02-18 21:46:08 +0100

    Added validation to the form

--- src/components/addNewPrinter.vue
+++ src/components/addNewPrinter.vue
@@ -3,12 +3,15 @@
     <!-- DODAWANIE DRUKARKI DO BAZY -->
     <h2>Dodaj drukarkę</h2>
     <form @submit.prevent="addPrinter">
+      <span v-if="!nameValid" style="color: red;">Nazwa musi zawierać 8 znaków.</span>
+      <span v-if="!modelValid" style="color: red;">Pole "Model" nie moze byc puste</span>
+      <span v-if="!serialValid" style="color: red;">Numer seryjny do wypełnienia</span>
       <div>
-        <label for="printerName">Printer Name:</label>
+        <label for="printerName" :style="{ color: nameValid ? 'green' : 'red' }">Printer Name:</label>
         <input type="text" id="printerName" placeholder="KON1L001" v-model="printer.printerName" />
       </div>
       <div>
-        <label for="model">Model:</label>
+        <label for="model" :style="{ color: modelValid ? 'green' : 'red' }">Model:</label>
         <select id="model" v-model="printer.model">
           <option value="">--brak--</option>
           <option value="QLn620">QLn620</option>
@@ -17,7 +20,7 @@
         </select>
       </div>
       <div>
-        <label for="printerSerialNumber">Serial Number:</label>
+        <label for="printerSerialNumber" :style="{ color: serialValid ? 'green' : 'red' }">Serial Number:</label>
         <input type="text" id="printerSerialNumber" placeholder="S1203120312031" v-model="printer.serialNumber" />
       </div>
       <button type="submit">Dodaj skaner</button>
@@ -34,12 +37,32 @@ export default {
         printerName: 'KON1L',
         model: '',
         serialNumber: 'Q123123123'
-      }
+      },
+      nameValid: false,
+      modelValid: false,
+      serialNumber: false,
     }
+  },
+  watch: {
+    'printer.printerName': function (newVal) {
+      this.nameValid = newVal.length === 8;
+    },
+    'printer.model': function (newVal) {
+      this.modelValid = newVal.lenght !== 0;
+    },
+    'printer.serialNumber': function (newVal) {
+      this.serialValid = newVal.length >= 12;
+    },
   },
   methods: {
     addPrinter() {
-      this.$emit('addNewPrinter', this.printer);
+      // Walidacja przed zwróceniem urządzeń
+      if(this.nameValid && this.modelValid && this.serialValid) {
+        this.$emit('addNewPrinter', this.printer);
+      } else{
+        alert("wypełnij poprawnie wszytkie pola!");
+        return;
+      }
     }
   }
 }
