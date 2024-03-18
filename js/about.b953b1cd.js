5fadd90339a75d0d0d07aa5212974bd2edc9af1a
ErykFryderyk <grzeniu098@gmail.com>
2024-03-18 06:58:33 +0100

    production version gh

--- js/about.b953b1cd.js
+++ js/about.b953b1cd.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkwarehouse_equipment_management"]=self["webpackChunkwarehouse_equipment_management"]||[]).push([[443],{5368:function(e,n,u){u.r(n),u.d(n,{default:function(){return f}});var a=u(3396);const t={class:"about"},s=(0,a._)("h1",null,"This page is about devices",-1),r=[s];function i(e,n){return(0,a.wg)(),(0,a.iD)("div",t,r)}var c=u(89);const o={},h=(0,c.Z)(o,[["render",i]]);var f=h}}]);
-//# sourceMappingURL=about.c1c2feb0.js.map
+//# sourceMappingURL=about.b953b1cd.js.map
